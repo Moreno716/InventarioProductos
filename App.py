dd26550981efd0b4edc599129ed1df8c075f62e5
moreno716 <juandiegomc00@gmail.com>
2024-04-19 17:49:54 -0500

    se agrega configuracion db

--- App.py
+++ App.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'inventario'
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    return 'PRUEBA'
+
+@app.route('/addProduct')
+def addProduct():
+    return 'AGREGAR PRODUCTO'
+
+@app.route('/editProduct')
+def editProduct():
+    return 'EDITAR PRODUCTO'
+
+@app.route('/deleteProduct')
+def deleteProduct():
+    return 'ELIMINAR PRODUCTO'
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
