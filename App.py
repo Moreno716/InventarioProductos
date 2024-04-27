c48998a921cc9f1068d50e389370bbc5e1ec38d9
moreno716 <juandiegomc00@gmail.com>
2024-04-27 11:24:09 -0500

    Proyecto terminado

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -8,21 +8,57 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'inventario'
 mysql = MySQL(app)
 
+app.secret_key = 'mysecretkey'
+
 @app.route('/')
 def Index():
-    return 'PRUEBA'
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM products')
+    data = cur.fetchall()
+    return render_template('index.html', products = data)
 
-@app.route('/addProduct')
+@app.route('/addProduct', methods=['POST'])
 def addProduct():
-    return 'AGREGAR PRODUCTO'
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        value = request.form['value']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO products (nombre, valor) VALUES (%s, %s)', (fullname, value))
+        mysql.connection.commit()
+        flash('Producto agregado!')
 
-@app.route('/editProduct')
-def editProduct():
-    return 'EDITAR PRODUCTO'
+        return redirect(url_for('Index'))
 
-@app.route('/deleteProduct')
-def deleteProduct():
-    return 'ELIMINAR PRODUCTO'
+@app.route('/edit/<id>')
+def getProduct(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM products WHERE id = %s', (id))
+    data = cur.fetchall()
+    return render_template('editProduct.html', product = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def updateProduct(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        value = request.form['value']
+        cur= mysql.connection.cursor()
+        cur.execute("""
+            UPDATE products
+            SET nombre = %s,
+                valor = %s
+            WHERE id = %s
+        """, (fullname, value, id))
+        mysql.connection.commit()
+        flash('Producto actualizado!')
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id>')
+def deleteProduct(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM products WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Producto eliminado!')
+    return redirect(url_for('Index'))
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
